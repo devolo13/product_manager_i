a823af42ec44f1c5ff49231aac9af7bc113a9b7a
devolo13 <devolo13@gmail.com>
2023-05-16 15:05:33 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import thing from place
+import ProductForm from "./components/ProductForm";
+// const ProductController = require('../controllers/product.controller');
+// import {Routes, Route, Link} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">Product Manager</h1>
+      <ProductForm/>
+    </div>
+  );
+}
+
+export default App;
